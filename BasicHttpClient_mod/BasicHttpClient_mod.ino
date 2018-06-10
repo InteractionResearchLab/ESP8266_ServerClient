ce20dc82ada3a2a6f183d672011a426c0242d8a5
Luis Fraguada <luis@mcneel.com>
2018-06-10 23:21:28 +0200

    Adding working server and client code.

---
+++ BasicHttpClient_mod/BasicHttpClient_mod.ino
@@ -0,0 +1,84 @@
+/**
+   BasicHTTPClient.ino
+
+    Created on: 24.05.2015
+
+*/
+
+#include <Arduino.h>
+
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+
+#include <ESP8266HTTPClient.h>
+
+ESP8266WiFiMulti WiFiMulti;
+
+void setup() {
+
+  Serial.begin(115200);
+  // Serial.setDebugOutput(true);
+
+  Serial.println();
+  Serial.println();
+  Serial.println();
+
+  for (uint8_t t = 4; t > 0; t--) {
+    Serial.printf("[SETUP] WAIT %d...\n", t);
+    Serial.flush();
+    delay(1000);
+  }
+
+  WiFi.mode(WIFI_STA);
+
+  //Change this to the Access Point SSID and PASSWORD
+  WiFiMulti.addAP("ASHKAN", "thereisnospoon");
+
+}
+
+void loop() {
+  // wait for WiFi connection
+  if ((WiFiMulti.run() == WL_CONNECTED)) {
+
+    Serial.print("[HTTP] begin...\n");
+
+    //here you can use the sendRequest method to send the req to any of your defined URLs
+    sendRequest("http://192.168.4.1/");
+    sendRequest("http://192.168.4.1/inline");
+    sendRequest("http://192.168.4.1/temperature");
+
+  }
+
+  delay(1000);
+}
+
+void sendRequest(String url) {
+
+  HTTPClient http;
+  
+  http.begin(url); //HTTP
+
+  Serial.print("[HTTP] GET...\n");
+  // start connection and send HTTP header
+  int httpCode = http.GET();
+
+  // httpCode will be negative on error
+  if (httpCode > 0) {
+    // HTTP header has been send and Server response header has been handled
+    Serial.printf("[HTTP] GET... code: %d\n", httpCode);
+
+    // file found at server
+    if (httpCode == HTTP_CODE_OK) {
+      String payload = http.getString();
+      Serial.println(payload);
+    }
+  } else {
+    Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+  }
+
+  http.end();
+
+}
+
+
+
